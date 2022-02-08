b48e2006e6b8a18d7b2b6dd52597e9220eaf3889
JimmyD72 <daileyj72@gmail.com>
2022-02-07 23:26:32 -0600

    new repo

---
+++ src/components/Button.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { btn, curved, white } from "./Button.module.scss";
+
+const Button = ({ children, isWhite, isCurved, styles, ...rest }) => {
+    if (isWhite) {
+        return (
+            <button
+                className={`${btn} ${white} ${isCurved ? curved : ""} ${
+                    styles ? styles : ""
+                }`}
+                {...rest}
+            >
+                {children}
+            </button>
+        );
+    } else {
+        return (
+            <button
+                className={`${btn}  ${isCurved ? curved : ""} ${
+                    styles ? styles : ""
+                }`}
+                {...rest}
+            >
+                {children}
+            </button>
+        );
+    }
+};
+
+
